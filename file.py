dcd0164b37044be2b5c6f170679c5ce6bf2f3b7f
AsyaTiho <generals761@gmail.com>
2022-03-08 04:44:33 +0000

    finished the function

--- file.py
+++ file.py
@@ -5,13 +5,12 @@ data = pd.read_csv("data/usdrub_d.csv")
 #
 # data.query('Date')
 def calc_poor_ratio(price, year):
-    ratio = 0.5
-    year_rows = data[data['Date'].str.contains(year)]['Close'].mean()
-    print(year_rows)
+    coefficient_year = data[data['Date'].str.contains(year)]['Close'].mean()
+    coefficient_2022 = data.iloc[-1]['Close']
+    ratio = coefficient_2022 / coefficient_year
     return ratio
 
-
-test_price = '10000'
-test_year = '2017'
+test_year = '2021'
+test_price = 15000
 r = calc_poor_ratio(test_price, test_year)
 print(r)
