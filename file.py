1d5a537677d66546e77c766a4bb1b0446f1d42b0
AsyaTiho <generals761@gmail.com>
2022-03-14 05:33:38 +0000

    imported file function

--- file.py
+++ file.py
@@ -2,25 +2,10 @@
 import pandas as pd
 data = pd.read_csv("data/usdrub_d.csv")
 
-#
+
 # data.query('Date')
 def calc_poor_ratio(price, year):
     coefficient_year = data[data['Date'].str.contains(year)]['Close'].mean()
     coefficient_2022 = data.iloc[-1]['Close']
     ratio = coefficient_2022 / coefficient_year
     return ratio
-
-test_year = '2021'
-test_price = 15000
-r = calc_poor_ratio(test_price, test_year)
-print(r)
-
-"""
-TODO
-1. How to import "*.py" files in python Anna: if the file in the same directory, you can just import 'name of the file'
-2. How to call a function defined in other file. (function is separated from its usage) Anna: you can use from 'name of the file' import 'function'
-3. Where python looks for files when you try to import something. Anna: I guess it searches through the directory, and it can only find it if its in the same directory. To find the file in a different folder, we need to indicate the entire path to it. For example, ./name of the folder/ name of the file.
-4. What happens when python imports a file. Anna: It employs the function of that file?
-5. Should you delete lines 13-16 or move to separate file (eg test_poor_ratio.py), or 
-   should you keep them? Anna: It doesnt seem like a necessary action to move them to a different file
-"""
--- webserver/main.py
+++ webserver/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory, request, jsonify
 from loguru import logger as log
 import sys, os
+from file import calc_poor_ratio
 
 DIRECTORY = './webserver/web'
 API_EP = '/api/v1'
@@ -10,8 +11,9 @@ app = Flask("Webserver", static_url_path='/', static_folder=DIRECTORY)
 def poor_ratio():
     log.info('Request: {}', request)
     log.info('Request json: {}', request.json)
-    import random
-    ratio = random.random()
+    year = request.json['year']
+    price = request.json['price']
+    ratio = calc_poor_ratio(price, year)
     return jsonify({"ratio": ratio})
 
 @app.route('/')
@@ -24,6 +26,8 @@ def main():
     PORT = int(os.environ.get('PORT', 5000))
     log.info('Running on {}:{}', HOST, PORT)
     app.run(host=HOST, port=PORT)
+
+
 
 main()
 
